97596c714c2449b2b513c5d1ff1c6d701c3d25f6
ryananicholson <ryananicholson@gmail.com>
2020-02-12 11:21:26 -0500

    Add verbosity option and cleaned up regex list

--- profane.py
+++ profane.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--regexes", help="File with the profane regular expressions", required=True)
 parser.add_argument("-s", "--source", help="Source file to be audited", required=True)
 parser.add_argument("-d", "--destination", help="Destination file to write clean words to", required=True)
+parser.add_argument("-v", "--verbose", help="Display clean lines as well", action="store_true")
 args = parser.parse_args()
 
 f = open(args.source, "r")
@@ -20,7 +21,9 @@ for line in f:
     for regex in badwords:
         m = re.search(regex, line)
         if m is not None:
-            print("Found! " + line)
+            print("\033[31m[-] Found: \033[0m" + line)
             break
     if m is None:
+        if args.verbose:
+            print("\033[32m[+] Clean: \033[0m" + line) 
         w.write(line)
