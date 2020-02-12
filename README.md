3fdfcc05e4af8def4aac31ba8794694fdd329c19
ryananicholson <ryananicholson@gmail.com>
2020-02-12 10:14:39 -0500

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # profane
-# profane
-# profane
+
+Very simple (and probably inefficient) Python script to crawl a file and look for profanity. What is deemed profane is in a regex file (**regexes.txt** is a good one to start with for *most* English profanity).
+
+Has been tested with both Python2 and Python3.
+
+## Usage
+
+Very easy to use
+
+```
+python profane.py -r regexes.txt -s potential-bad-words.txt -d clean.txt
+```
+
+| Command-line Option | Description               |
+|---------------------|---------------------------|
+| -r/--regexes        | File with profane regexes |
+| -s/--source         | File to be audited        |
+| -d/--destination    | Clean file to save to     |
---
+++ profane.py
@@ -0,0 +1,26 @@
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--regex", help="File with the profane regular expressions", required=True)
+parser.add_argument("-s", "--source", help="Source file to be audited", required=True)
+parser.add_argument("-d", "--destination", help="Destination file to write clean words to", required=True)
+args = parser.parse_args()
+
+f = open(args.source, "r")
+w = open(args.destination, "w")
+r = open(args.regex, "r")
+badwords = []
+
+for line in r:
+    badwords.append(line)
+
+for line in f:
+    m = ""
+    for regex in badwords:
+        m = re.search(regex, line)
+        if m is not None:
+            print("Found! " + line)
+            break
+    if m is None:
+        w.write(line)
